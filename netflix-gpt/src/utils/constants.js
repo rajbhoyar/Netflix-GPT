35ecd60ebed7c18d604be24e54a088b39bbb30e2
Raj Bhoyar <Rajbhoyar21@gmail.com>
2024-10-06 13:56:42 +0530

    Enhanced the constants

--- netflix-gpt/src/utils/constants.js
+++ netflix-gpt/src/utils/constants.js
@@ -1,3 +1,6 @@
+export const NETFLIX_LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
 export const LOGO =
   "https://assets.nflxext.com/ffe/siteui/vlv3/4d2c5849-b306-4884-9036-6211f7ee0178/web/IN-en-20240930-TRIFECTA-perspective_1e1ca6cd-9e2d-4e9d-9e4b-ba0c2d3a0e31_small.jpg";
 
